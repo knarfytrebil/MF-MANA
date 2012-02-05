ed869b273186cd248247e26ffd05c3ffbc9668fc
Relaed <knarfytrebil@gmail.com>
2012-02-05 11:42:30 +0800

    63

--- lib/common.py
+++ lib/common.py
@@ -69,11 +69,11 @@ def DateToTimeStamp(date):
 	import time
 	x = datetime.datetime.strptime(date,"%Y-%m-%d")
 	x = x.timetuple()
-	return int(time.mktime(x))*1000 - 60000000
+	return int(time.mktime(x))*1000 - 63000000
 
 def HourToTimeStamp(date):
 	import datetime
 	import time
 	x = datetime.datetime.strptime(date,"%Y-%m-%d %H:%M:%S")
 	x = x.timetuple()
-	return int(time.mktime(x))*1000 - 60000000
+	return int(time.mktime(x))*1000 - 63000000
