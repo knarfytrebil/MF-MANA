e548fad5471a620342fd6d7bcfb0f4fafebab313
Relaed <knarfytrebil@gmail.com>
2012-02-05 11:45:48 +0800

    bars

--- static/themes/desktop/user/js/fun.js
+++ static/themes/desktop/user/js/fun.js
@@ -26,7 +26,7 @@ function switchTab(identify,index,count,cnon,cnout) {
 function plot_overview(class_id,data){
     $.plot($(class_id), [data], {
         series: {
-            lines: { show: true, lineWidth: 1 },
+            bars: { show: true, lineWidth: 1 },
             shadowSize: 0
         },
         grid: { color: "#FFF" },
@@ -59,7 +59,7 @@ function plot_big(class_id,data){
 
     var options = {
         xaxis: { mode: "time" },
-        minTickSize: [1, "month"],
+        minTickSize: [1,"hour"],
         grid: { markings: weekendAreas }
     };
 
