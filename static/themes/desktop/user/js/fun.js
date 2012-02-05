2a6e9946c02fdc1bd198e38136dba75dda1af31f
Relaed <knarfytrebil@gmail.com>
2012-02-05 13:59:27 +0800

    Hover

--- static/themes/desktop/user/js/fun.js
+++ static/themes/desktop/user/js/fun.js
@@ -64,9 +64,49 @@ function plot_big(class_id,data){
         xaxis: { mode: "time" },
         grid: { 
             markings: weekendAreas,
-            backgroundColor: { colors: ["#fff", "#eee"] }
+            backgroundColor: { colors: ["#fff", "#eee"] },
+            hoverable: true
         }
     };
 
     $.plot($(class_id), [data], options);
+}
+
+
+//plot hover
+function showTooltip(x, y, contents) {
+    $('<div id="tooltip">' + contents + '</div>').css( {
+        position: 'absolute',
+        display: 'none',
+        top: y + 5,
+        left: x + 5,
+        border: '1px solid #fdd',
+        padding: '2px',
+        'background-color': '#fee',
+        opacity: 0.80
+    }).appendTo("body").fadeIn(200);
+}
+
+function BindToolTip(plottarget) {
+    var previousPoint = null;
+    $(plottarget).bind("plothover", function (event, pos, item) {
+        
+        if (item) {
+            if (previousPoint != item.dataIndex) {
+                previousPoint = item.dataIndex;
+                
+                $("#tooltip").remove();
+                var x = item.datapoint[0].toFixed(2),
+                    y = item.datapoint[1].toFixed(2);
+                
+                showTooltip(item.pageX, item.pageY,
+                            item.series.label + " of " + x + " = " + y);
+            }
+        }
+        else {
+            $("#tooltip").remove();
+            previousPoint = null;            
+        }
+        
+    });
 }
