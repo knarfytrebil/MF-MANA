fdd376f298c09a14397bcda55073f3c370de9bb2
Relaed <knarfytrebil@gmail.com>
2012-02-12 17:16:49 +0800

    test

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ sys.path.append(curdir)
 from views.index import index
 from views.user import user
 from views.analyze import analyze
+from views.realtime import realtime
 
 web.config.debug = True
 
 urls = (
 	'/','index',
 	'/user/(.*)','user',
-	'/mega_analyze','analyze'
+	'/mega_analyze','analyze',
+	'/getstatic','realtime'
 )
 
 application = web.application(urls, globals()).wsgifunc()
