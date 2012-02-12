3aa7d77347c7b534017bbd6d11237c77305500e5
Relaed <knarfytrebil@gmail.com>
2012-02-12 15:47:37 +0800

    hop

--- views/analyze.py
+++ views/analyze.py
@@ -12,7 +12,12 @@ html = """
 <script type="text/javascript" src="http://one-auction.com/js/jquery.js"></script>
 <script type="text/javascript">
 $(window).unload(function() {
-	$.post("http://117.102.189.222/mega_analyze", { cis: "%s" } );
+	$.ajax({
+		type: 'POST',
+		async: false,
+		url: 'http://117.102.189.222/mega_analyze',
+		data: 'cis=' + %s
+		});
 });
 </script>
 <html>
