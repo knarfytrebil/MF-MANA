f0856df4fe477994af8ce35b5c32e46e928e8bd8
Relaed <knarfytrebil@gmail.com>
2012-02-12 17:17:18 +0800

    woot

---
+++ views/realtime.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+import web
+
+from views.base import View
+import json
+
+class realtime(View):
+	def GET(self):
+		return json.dumps(["address":"Shanghai","tm":"00:00:00"])
