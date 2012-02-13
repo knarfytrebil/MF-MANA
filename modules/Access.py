0f5b12c30c5e46f97da01209d19f7cbb0e731aac
Relaed <knarfytrebil@gmail.com>
2012-02-13 21:28:03 +0800

    added stuff

---
+++ modules/Access.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Rate.py
+
+Created by 姚 远 on 2011-12-27.
+Copyright (c) 2011 ManaFlame. All rights reserved.
+"""
+from modules.base import Base
+
+class Access(Base):
+	"""docstring for Settings"""
+	def hi(self):
+		pass
--- views/realtime.py
+++ views/realtime.py
@@ -3,10 +3,12 @@ import os
 import web
 
 from views.base import View
+from modules.Access import Access
 import json
 
 class realtime(View):
-	def GET(self):
+	def GET(self,time):
+		access = Access('flame_balance','local')	
 		web.header('Content-Type', 'application/javascript')
 		d = json.dumps({"address": "Shanghai", "tm": "00:00:00" })
 		return 'gen_li(' + d + ');'
